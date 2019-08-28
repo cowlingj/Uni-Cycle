6a8917e211dd6f6551bfe22477234ff1488a1536
cowlingj <09cowlingj@gmail.com>
2019-08-28 12:43:54 +0100

    Fix CMS test fails for correct reason

--- integration-test/runner/test/cms-should.js
+++ integration-test/runner/test/cms-should.js
@@ -1,21 +1,13 @@
 import request from "request-promise-native"
 
 test('cms install succeds', async () => {
-  try {
-    const res = await request({
-      methid: 'GET',
-      uri: 'http://cms/install',
-      resolveWithFullResponse: true,
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
-    })
-    console.log(JSON.stringify(res))
-    expect(res.statusCode).toBe(200)
-  } catch (err) {
-    fail()
-  }
-});
+  const res = await request({
+    methid: 'GET',
+    uri: 'http://cms/install',
+    resolveWithFullResponse: true,
+  })
+  expect(res.statusCode).toBe(200)
+  expect(res.body).not.toMatch(/error/i)
+})
 
 
