2fddb58dbdc22b5f58b6254f8adef894ef273ca0
cowlingj <09cowlingj@gmail.com>
2019-08-31 14:22:57 +0100

    Add testing with kubernetes

--- integration-test/runner/test/cms-should.js
+++ integration-test/runner/test/cms-should.js
@@ -10,4 +10,13 @@ test('cms install succeds', async () => {
   expect(res.body).not.toMatch(/error/i)
 })
 
+test('cms get strings succeds', async () => {
+  const res = await request({
+    methid: 'GET',
+    uri: 'http://cms/api/collections/get/strings/',
+    resolveWithFullResponse: true,
+  })
+  expect(res.statusCode).toBe(200)
+  expect(res.body).toMatch('whatever the json should be')
+})
 
--- integration-test/test.sh
+++ integration-test/test.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env sh
 
-docker-compose up --build --abort-on-container-exit --exit-code-from test
-docker-compose down
+set -u
+
+clean() {
+  kubectl delete -Rf ./kubernetes
+  docker image rm -f cms.frontend.local:test mongodb.backend.local:test integration.test.local:test
+}
+trap clean EXIT
+
+clean
+
+docker build -t cms.frontend.local:test ../cms &
+docker build -t mongodb.backend.local:test ./mongodb &
+docker build -t integration.test.local:test ./runner &
+
+wait
+
+kubectl apply -Rf ./kubernetes
+
+sleep 30
+
+kubectl run integration-test \
+  --attach \
+  --rm \
+  --generator=run-pod/v1 \
+  --restart=Never \
+  --image=integration.test.local:test
+
