25710ec247be3cb26081fd4de18d304b75f7aab7
cowlingj <09cowlingj@gmail.com>
2019-09-03 23:32:45 +0100

    Fix choose keystonejs 4 as cms

--- cms/integration-test/test.sh
+++ cms/integration-test/test.sh
@@ -9,8 +9,7 @@ trap clean EXIT
 
 clean
 
-docker build -t cms.frontend.local:test ../cms/main &
-docker build -t init-cms.frontend.local:test ../cms/init &
+docker build -t cms.frontend.local:test ../cms &
 docker build -t store.frontend.local:test ../store &
 docker build -t mongodb.backend.local:test ./mongodb &
 docker build -t integration.test.local:test ./runner &
@@ -22,8 +21,11 @@ helm dependency update ./deploy
 helm install --name integration-test ./deploy || exit 1
 
 # let initialisation finish
-kubectl wait --timeout 30s --for=condition=Ready --all pods || exit 1
-kubectl wait --timeout 300s --for=condition=complete job/init-cms || exit 1
+kubectl wait --timeout 60s --for=condition=Ready --all pods || exit 1
+
+echo 'READY'
+
+sleep 300
 
 # run tests
 kubectl run integration-test \
