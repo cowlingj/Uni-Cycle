3bb42c5950e9b6772645a3cb5b29168a10467e09
cowlingj <09cowlingj@gmail.com>
2019-09-24 20:01:59 +0100

    Add helm repo

--- deploy/inf/helm/ingress.tf
+++ deploy/inf/helm/ingress.tf
@@ -2,7 +2,7 @@ resource "helm_release" "gateway" {
   depends_on = [ helm_release.istio ]
 
   name       = "gateway"
-  chart      = "${path.root}/../charts/gateway"
+  chart      = "${path.root}/../../helm/repo/gateway-0.0.1.tgz"
   version    = "0.0.1"
   namespace  = var.namespaces.main
 
--- deploy/inf/helm/store.tf
+++ deploy/inf/helm/store.tf
@@ -2,7 +2,7 @@ resource "helm_release" "store" {
   depends_on = [ helm_release.istio ]
 
   name       = "store"
-  chart      = "${path.root}/../charts/store"
+  chart      = "${path.root}/../../helm/repo/store-0.0.1.tgz"
   version    = "0.0.1"
   namespace  = var.namespaces.main
 
