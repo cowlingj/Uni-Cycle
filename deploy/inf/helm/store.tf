11648e74586b6f5d30814cea95b128c220627cd1
cowlingj <09cowlingj@gmail.com>
2019-10-03 01:00:43 +0100

    Fix helm and tf config

--- deploy/inf/helm/store.tf
+++ deploy/inf/helm/store.tf
@@ -2,7 +2,7 @@ resource "helm_release" "store" {
   depends_on = [ helm_release.istio ]
 
   name       = "store"
-  chart      = "${path.root}/../../helm/repo/store-0.0.1.tgz"
+  chart      = "${path.root}/../../store/charts/store/"
   version    = "0.0.1"
   namespace  = var.namespaces.main
 
@@ -14,5 +14,20 @@ resource "helm_release" "store" {
   set {
     name = "cms.svc.api.namespace"
     value = var.namespaces.main
+  }
+
+  set {
+    name = "cms.mongodb.version"
+    value = "0.0.1"
+  }
+
+  set {
+    name = "cms.mongodb.port"
+    value = "8080"
+  }
+
+  set {
+    name = "cms.cms.mongodbUrl"
+    value = "mongodb://cms:cms@mongodb.${var.namespaces.main}.svc.cluster.local:8080/cms"
   }
 }
