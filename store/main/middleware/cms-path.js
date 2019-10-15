786675bc1de14dbf46bc320e00aacf3edba18223
cowlingj <09cowlingj@gmail.com>
2019-10-15 23:57:32 +0100

    Fix cms external url

--- store/main/middleware/cms-path.js
+++ store/main/middleware/cms-path.js
@@ -1,8 +1,10 @@
 export default (context) => {
   if (process.server) {
     // TODO: extract path
-    context.app.$env.CMS_EXTERNAL_URL = `${context.req.protocol || 'http'}://${
-      context.req.headers.host
-    }/cms/graphql`
+    context.beforeNuxtRender(({ nuxtState }) => {
+      nuxtState.env.CMS_EXTERNAL_URL = `${context.req.protocol || 'http'}://${
+        context.req.headers.host
+      }/cms/graphql`
+    })
   }
 }
