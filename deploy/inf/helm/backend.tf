28eebf67dac1be25f70aecd016ed1008c1f9bf30
cowlingj <09cowlingj@gmail.com>
2019-11-15 20:30:10 +0000

    Fix mongodb cms auth

--- deploy/inf/helm/backend.tf
+++ deploy/inf/helm/backend.tf
@@ -1,15 +1,19 @@
-resource "random_string" "cms_db_password" {
+resource "random_password" "cms_db_password" {
   length = 32
-  special = false
+  special = true
+  override_special = "$-_.+!*'()" # don't include things to confuse uris
+  keepers = {
+    storage_id = var.pvc_name
+  }
 }
 
-resource "random_string" "root_db_password" {
+resource "random_password" "root_db_password" {
   length = 32
-  special = false
-}
-
-output "rootPw" {
-  value = random_string.root_db_password.result
+  special = true
+  override_special = "$-_.+!*'()" # don't include things to confuse uris
+  keepers = {
+    storage_id = var.pvc_name
+  }
 }
 
 variable "cms_db_username" {
@@ -26,7 +30,7 @@ resource "helm_release" "backend" {
 
   depends_on = [ var._depends_on ]
 
-  timeout = 600
+  timeout = 900
   
   name       = "ecommerce-backend"
   repository = data.helm_repository.github_master.name
@@ -69,22 +73,12 @@ resource "helm_release" "backend" {
 
   set_sensitive {
     name = "mongodb-config.rootPassword"
-    value = random_string.root_db_password.result
+    value = random_password.root_db_password.result
   }
 
   set {
-    name = "mongodb.image.registry"
-    value = "gcr.io"
-  }
-
-  set {
-    name = "mongodb.image.repository"
-    value = "gke-test-253221/mongodb.data"
-  }
-
-  set {
-    name = "mongodb.image.tag"
-    value = "0.0.1"
+    name = "mongodb.volumePermissions.enabled"
+    value = true
   }
 
   set_sensitive { # TODO: integrate into backend (use an override host flag)
@@ -100,5 +94,10 @@ resource "helm_release" "backend" {
   set {
     name = "cms.users.list[0].password"
     value = "admin1"
+  }
+
+  set {
+    name = "cms.init.mongodb.cms.mongodbPassword"
+    value = random_password.cms_db_password.result
   }
 }
