4fadf3afbc3233f70e5cf90bf8638b2bb0224daf
cowlingj <09cowlingj@gmail.com>
2020-01-06 21:27:51 +0000

    Debug product endpoint

--- store/main/src/middleware/products-path.js
+++ store/main/src/middleware/products-path.js
@@ -1,6 +1,13 @@
 export default (context) => {
-  if (process.server && context.app.$env.PRODUCTS_EXTERNAL_ENDPOINT === undefined) {
+  console.log(`server: ${process.server}`)
+  console.log(`external: ${JSON.stringify(context.app.$env.PRODUCTS_EXTERNAL_ENDPOINT)}`)
+
+  if (
+    process.server &&
+    context.app.$env.PRODUCTS_EXTERNAL_ENDPOINT === undefined
+  ) {
     context.app.$env.PRODUCTS_EXTERNAL_ENDPOINT = `${context.req.protocol ||
       'http'}://${context.req.headers.host}`
+    console.log(`set external: ${context.app.$env.PRODUCTS_EXTERNAL_ENDPOINT}`)
   }
 }
